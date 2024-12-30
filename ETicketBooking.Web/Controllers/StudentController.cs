4591289e682152245b61c8ef2110521841e21a1a
newpheeraphatflowaccount <165787650+newpheeraphatflowaccount@users.noreply.github.com>
2024-12-30 12:54:39 +0700

    Add project files.

---
+++ ETicketBooking.Web/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using ETicketBooking.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicketBooking.Web.Controllers
+{
+	public class StudentController : Controller
+	{
+		// {base_url}/Students/Index - End, HttpRequest
+		public IActionResult Index()
+		{
+			List<Student> student = new List<Student>();
+			student.Add(new Student { Id = 1, Name = "John" });
+			student.Add(new Student { Id = 2, Name = "Smith" });
+			return View();
+		}
+	}
+}
