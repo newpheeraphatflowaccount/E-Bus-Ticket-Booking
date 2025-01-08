64e307d40e74bf49e2527597985c189dddfa3f5b
newpheeraphatflowaccount <165787650+newpheeraphatflowaccount@users.noreply.github.com>
2025-01-08 12:35:17 +0700

    feat: add dashboard view component

---
+++ ETicketBooking.UI/ViewComponents/DashboardViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicketBooking.Repositories.Interfaces;
+using ETicketBooking.UI.ViewModels.Dashboard;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicketBooking.UI.ViewComponents
+{
+	public class DashboardViewComponent : ViewComponent
+	{
+		private readonly IDepartmentRepo _departmentRepo;
+		private readonly IStudentRepo _studentRepo;
+		private readonly IEmployeeRepo _employeeRepo;
+
+		public DashboardViewComponent(IDepartmentRepo departmentRepo,
+			IStudentRepo studentRepo,
+			IEmployeeRepo employeeRepo)
+		{
+			_departmentRepo = departmentRepo;
+			_studentRepo = studentRepo;
+			_employeeRepo = employeeRepo;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var vm = new Dashboard
+			{
+				TotalDepartment = _departmentRepo.GetAll().GetAwaiter().GetResult().Count(),
+				TotalStudent = _studentRepo.GetAll().GetAwaiter().GetResult().Count(),
+				TotalEmployee = _employeeRepo.GetAll().GetAwaiter().GetResult().Count()
+			};
+			return View(vm);
+		}
+	}
+}
---
+++ ETicketBooking.UI/ViewModels/Dashboard/Dashboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicketBooking.UI.ViewModels.Dashboard
+{
+	public class Dashboard
+	{
+		public int TotalEmployee { get; set; }
+		public int TotalStudent { get; set; }
+		public int TotalDepartment { get; set; }
+	}
+}
